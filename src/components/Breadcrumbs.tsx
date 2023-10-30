c9dc020d374a4bef63b0cef929d0b778eb4bdbaa
Daniel Villajuan <danielvillajuan@hotmail.com>
2023-10-30 18:07:09 -0300

    Breadcrumbs component

---
+++ src/components/Breadcrumbs.tsx
@@ -0,0 +1,13 @@
+export default function Breadcrumbs (): JSX.Element {
+  return (
+    <div className='row'>
+      <div className='cont_tags com-secondary-tag hlp-marginBottom-20'>
+        <a>Platos principales</a>
+        <a>Cerdo</a>
+        <a>Papas</a>
+        <a>Date un gustito</a>
+        <a>La familia</a>
+      </div>
+    </div>
+  )
+}
